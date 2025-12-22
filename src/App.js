3ca5fe290f584d2924dbc01bd0919ef3a3f79167
Zreel <zareelbwd@gmail.com>
2025-12-22 15:30:58 +0530

    image changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import large from "./img/large.png";
 
-import mobile from "./img/mobile.png"
+import mobile from "./img/mobile2.png"
 import axios from "axios";
 
 function App() {
